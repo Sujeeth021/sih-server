454748ea596086873f06d494f63ede008f73ce4c
Ram-256bit <72127084+Ram-256bit@users.noreply.github.com>
2024-09-08 15:30:24 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const path = require('path');
+// const path = require('path');
 
 let latestMessage = "test1";
 let latestImageData = null; // To store the image data
